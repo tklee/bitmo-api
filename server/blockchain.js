c28f1dc31074e9b693c56da989eefddd3bfcb1a5
Dickson Tam <dickson.tam@icloud.com>
2014-11-01 15:44:38 -0700

    Added ping endpoint to provide information on the status of the blockchain service.

--- server/blockchain.js
+++ server/blockchain.js
@@ -1,11 +1,34 @@
 var agent = require('superagent');
 
+function ping(cb) {
 
-function ping() {
-  return false;
+  agent.get('https://blockchain.com')
+  .end(function(res) {
+    console.log(res);
+    if (res.ok) {
+      cb(true)
+    } else {
+      cb(false)
+    };
+  });
+}
+
+function init(app) {
+  app.get('/ping', function (req, res) {
+    blockchain.ping(function(bRes) {
+      if (bRes) {
+        res.send('Ping succeeded');
+      } else {
+        res.send('Ping failed');
+      }
+    });
+  })
 }
 
 
+
+
 module.exports = {
-  ping: ping
+  ping: ping,
+  init: init
 }
