03937e879cc3d1bc99bd6cc37419ff00c2c877b3
Dickson Tam <dickson.tam@icloud.com>
2014-11-01 15:47:30 -0700

    Removed merge artifact.

--- server/app.js
+++ server/app.js
@@ -11,7 +11,6 @@ app.get('/', function (req, res) {
   myUser = { "number": "4081112222", "guid": "new_guid", "address":"new_adress", "link":"new_link", "name":"new_name", "password":"new_password" };
   u.addUser(myUser);
 })
->>>>>>> a5a70d26e5b6beb5099a1fc0e35c975d236abe19
 
 var server = app.listen(3000, function () {
 
