4c879736c1b24cdc06df8a90a0ae209402059922
Theodore Lee <theo@shiphawk.com>
2014-11-01 15:58:26 -0700

    added method to find by number

--- server/app.js
+++ server/app.js
@@ -6,8 +6,15 @@ var u = require('./users.js')
 app.get('/', function (req, res) {
   res.send('Hello World!')
 
-  myUser = { "number": "4081112222", "guid": "new_guid", "address":"new_adress", "link":"new_link", "name":"new_name", "password":"new_password" };
+  //myUser = { "number": "4081112222", "guid": "new_guid", "address":"new_adress", "link":"new_link", "name":"new_name", "password":"new_password" };
+  myUser = { "number": "4082223333", "guid": "new_guid", "address":"new_adress", "link":"new_link", "name":"new_name", "password":"new_password" };
   u.addUser(myUser);
+})
+
+app.get('/testSearch', function (req, res) {
+  res.send('Hello World!')
+
+  res.send(u.findUserByNumber("4082223333").number);
 })
 
 var server = app.listen(3000, function () {
--- server/users.js
+++ server/users.js
@@ -8,16 +8,23 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test');
 
+var User = mongoose.model('User', { number: String, guid: String, address: String, link: String, name: String, password: String });
 function addUser(aUser) {
-  var User = mongoose.model('User', { number: String, guid: String, address: String, link: String, name: String, password: String });
-  var user = new User({ name: aUser.number, guid: aUser.guid, address: aUser.address, link: aUser.link });
+  var user = new User({ number: aUser.number, guid: aUser.guid, address: aUser.address, link: aUser.link, name: aUser.name, password: aUser.password });
   user.save(function (err) {
     if (err) // ...
     console.log('Failed to save');
   });
 }
 
-module.exports = {
-  addUser: addUser
-}
+function findUserByNumber(aNumber) {
+  User.find({ "number": aNumber }, function (err, docs) {
+    console.log(docs)
+    return docs[0];
+  })
+};
 
+module.exports = {
+  addUser: addUser,
+  findUserByNumber: findUserByNumber
+}
