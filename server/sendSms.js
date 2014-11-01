63db983b4fee8eff13ed5a35c9a9cf84fbe5be53
Jonathan Kassan <jonathankassan@Jonathans-MacBook-Pro.local>
2014-11-01 16:45:39 -0700

    user Object instead of indiv params

--- server/sendSms.js
+++ server/sendSms.js
@@ -3,25 +3,38 @@ var accountSid = 'AC2df5e5b6e67043b626ae32dcec610370';
 var authToken = '147a8dd021c103e6140629c3712b0f75';
 var client = require('twilio')(accountSid, authToken);
  
-function SignUpSMS(phoneNumber, userName){
+function SignUpSMS(User){
 	client.messages.create({
-    body: "Hello " + userName + " You have requested to make a Bitmo ",
-    to:   "+1" + phoneNumber,
+    body: "Hello " + User.name + " You have requested to make a Bitmo ",
+    to:   "+1" + User.phoneNumber,
     from: "+14243325880"
 }, function(err, message) {
     process.stdout.write(message.sid);
 });
 }
 
-function NewAccountSMS(phoneNumber, senderName, password){
+function NewAccountSMS(Sender , Receiver){
 	client.messages.create({
-    body: "Hey! your friend " + senderName + " just sent you money on bitmo. Got to Bitmo.com Login with your phone number and password( " + password + " ) to retrieve your mula",
-    to:   "+1" + phoneNumber,
+    body: "Hey! your friend " + Sender.name + " just sent you money on bitmo. Go to Bitmo.com Login with your phone number and password( " + Receiver.password + " ) to own your coin",
+    to:   "+1" + Receiver.phoneNumber,
     from: "+14243325880"
 }, function(err, message) {
     process.stdout.write(message.sid);
 });
 }
 
-// SignUpSMS("8184377808", "Sol");
-//NewAccountSMS("8184377808", "Dickson", "pwa34rd");
+// Receiver = {
+// 	name: "User1",
+// 	phoneNumber: "8184377808",
+// 	password: "pa22w4rd"
+// };
+
+// Sender = {
+// 	name: "Sender User",
+// 	phoneNumber: "8184377808",
+// 	password: "pa22w4rd"
+// };
+
+// SignUpSMS(Receiver);
+
+// NewAccountSMS(Sender, Receiver);
