22937faa8cef111f7cf2ea25a1afde906b23b708
Theodore Lee <theo@shiphawk.com>
2014-11-01 18:32:35 -0700

    Changed to write retrieved document to response

--- server/users.js
+++ server/users.js
@@ -9,6 +9,7 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test');
 
 var User = mongoose.model('User', { number: String, guid: String, address: String, link: String, name: String, password: String });
+
 function addUser(aUser) {
   var user = new User({ number: aUser.number, guid: aUser.guid, address: aUser.address, link: aUser.link, name: aUser.name, password: aUser.password });
   user.save(function (err) {
@@ -18,14 +19,18 @@ function addUser(aUser) {
 }
 
 function init(app) {
-  app.get('/findTest', function (req, res) {
-    findUserByNumber("4082223333", function(bRes) {
+  app.get('/findTest', function (req, res, next) {
+    findUserByNumber("4082223333", function(bRes, result) {
       if (bRes) {
-        res.send('find succeeded');
+        req.user = result;
+	cb(req.user);
+      	next();
       } else {
-        res.send('find failed');
+	res.send(404, 'Invalid request');
       }
     });
+}, function(req, res) {
+	res.send(req.user);
   });
 }
 
@@ -33,9 +38,9 @@ function findUserByNumber(aNumber, cb) {
   User.find({ "number": aNumber }, function (err, docs) {
     // dangerous to keep waiting
     if (docs[0]) {
-      cb(true)
+      cb(true, docs[0])
     } else {
-      cb(false)
+      cb(false, docs[0])
     }
   })
 };
