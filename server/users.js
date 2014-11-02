44295562a00f2a206c4ec2abab0ce1d53597acb5
Sol Tran <sol@shiphawk.com>
2014-11-02 06:43:27 -0800

    customize mongoose configs to work on heroku

--- server/users.js
+++ server/users.js
@@ -7,7 +7,18 @@
 
 
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test');
+var uriUtil = require('mongodb-uri');
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+
+
+
+var mongodbUri = 'mongodb://heroku_app31209122:j2qhlnv9ad4jpatu57gg69vefq@ds049180.mongolab.com:49180/heroku_app31209122';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+
+mongoose.connect(mongooseUri, options);
 
 var User = mongoose.model('User', { number: String, guid: String, address: String, link: String, name: String, password: String });
 
